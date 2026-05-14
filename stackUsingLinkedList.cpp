5660fad01574de92caa4714152dd855af846afb9
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-05-14 10:52:00 +0700

    menambahkan validasi pilihan menu dan format output program

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -123,3 +123,11 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
+
+            default:
+            cout << "invalid choice. Try again." << endl;
+            break;
+        }
+
+        cout << endl;
+    }
